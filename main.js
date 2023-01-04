566c65b1bf436bdc6b0d611e10a6c662e3dfafcf
MatteoBax <matte.mb2006.9990@gmail.com>
2023-01-04 11:11:13 +0100

    added main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+function zFill(n){
+    return n.padStart('2', 0);
+}
+function convertToHex(r,g,b){
+    var hex = "";
+    if((r>=0 && r<=255) && (g>=0 && g<=255) && (b>=0 && b<=255)){
+        r = zFill(r.toString(16));
+        g = zFill(g.toString(16));
+        b = zFill(b.toString(16));
+        hex = "#" + r.toUpperCase() + g.toUpperCase() + b.toUpperCase();
+        return hex;
+    }else{
+        return false;
+    }
+    
+    
+}
+module.exports = {
+    convertToHex
+}
